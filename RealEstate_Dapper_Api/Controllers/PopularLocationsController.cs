cf4ca40197b8af2dace9304c3bc9f37cb20efcbb
Mustafa Cenan <54mustafacenan54@gmail.com>
2024-07-17 04:34:29 +0300

    popular locations consuming in admin completed

--- RealEstate_Dapper_Api/Controllers/PopularLocationsController.cs
+++ RealEstate_Dapper_Api/Controllers/PopularLocationsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Dtos.PopularLocationDtos;
 using RealEstate_Dapper_Api.Repositories.PopularLocationRepositories;
 
 namespace RealEstate_Dapper_Api.Controllers
@@ -20,6 +21,34 @@ namespace RealEstate_Dapper_Api.Controllers
         {
             var values = await _locationRepository.GetAllPopularLocationAsync();
             return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePopularLocation(CreatePopularLocationDto createPopularLocationDto)
+        {
+            _locationRepository.CreatePopularLocation(createPopularLocationDto);
+            return Ok("Popüler konum başarıyla oluşturuldu.");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePopularLocation(int id)
+        {
+            _locationRepository.DeletePopularLocation(id);
+            return Ok("Başarılı bir şekilde popüler konum silindi!");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatePopularLocation(UpdatePopularLocationDto updatePopularLocation)
+        {
+            _locationRepository.UpdatePopularLocation(updatePopularLocation);
+            return Ok("Başarılı bir şekilde popüler konum güncellendi!");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPopularLocation(int id)
+        {
+            var value = await _locationRepository.GetPopularLocation(id);
+            return Ok(value);
         }
     }
 }
--- RealEstate_Dapper_Api/Repositories/PopularLocationRepositories/PopularLocationRepository.cs
+++ RealEstate_Dapper_Api/Repositories/PopularLocationRepositories/PopularLocationRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using RealEstate_Dapper_Api.Dtos.PopularLocationDtos;
+using RealEstate_Dapper_Api.Dtos.ServiceDtos;
 using RealEstate_Dapper_UI.Models.DapperContext;
 
 namespace RealEstate_Dapper_Api.Repositories.PopularLocationRepositories
@@ -13,6 +14,30 @@ namespace RealEstate_Dapper_Api.Repositories.PopularLocationRepositories
             _context = context;
         }
 
+        public async void CreatePopularLocation(CreatePopularLocationDto createPopularLocationDto)
+        {
+            string query = "insert into PopularLocation (CityName, ImageUrl) values (@cityName, @imageUrl)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@cityName", createPopularLocationDto.CityName);
+            parameters.Add("@imageUrl", createPopularLocationDto.ImageUrl);
+
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async void DeletePopularLocation(int id)
+        {
+            string query = "Delete from PopularLocation Where LocationID=@locationID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@locationID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
         public async Task<List<ResultPopularLocationDto>> GetAllPopularLocationAsync()
         {
             string query = "Select * from PopularLocation";
@@ -20,6 +45,33 @@ namespace RealEstate_Dapper_Api.Repositories.PopularLocationRepositories
             {
                 var values = await connection.QueryAsync<ResultPopularLocationDto>(query);
                 return values.ToList();
+            }
+        }
+
+        public async Task<GetByIdPopularLocationDto> GetPopularLocation(int id)
+        {
+            string query = "select * from PopularLocation where LocationID=@locationID";
+            var parameters = new DynamicParameters();
+            parameters.Add("locationID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryFirstOrDefaultAsync<GetByIdPopularLocationDto>(query, parameters);
+                return values;
+            }
+        }
+
+        public async void UpdatePopularLocation(UpdatePopularLocationDto updatePopularLocationDto)
+        {
+            string query = "Update PopularLocation Set CityName=@cityName, ImageUrl=@imageUrl where LocationID=@locationID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@cityName", updatePopularLocationDto.CityName);
+            parameters.Add("@imageUrl", updatePopularLocationDto.ImageUrl);
+            parameters.Add("@locationID", updatePopularLocationDto.LocationID);
+
+
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
             }
         }
     }
