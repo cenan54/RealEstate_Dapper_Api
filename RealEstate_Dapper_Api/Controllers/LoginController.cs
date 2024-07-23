b997b3092533c9bcc1b5f042a75ccbdbfe649d9b
Mustafa Cenan <54mustafacenan54@gmail.com>
2024-07-23 18:30:09 +0300

    user login jwt generator edited

---
+++ RealEstate_Dapper_Api/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Dtos.LoginDtos;
+using RealEstate_Dapper_Api.Tools;
+using RealEstate_Dapper_UI.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public LoginController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(CreateLoginDto loginDto)
+        {
+            string query = "Select * from AppUser where Username=@username and Password=@password";
+            var parameters = new DynamicParameters();
+            parameters.Add("@password",loginDto.Password);
+            parameters.Add("@username",loginDto.Username);
+
+            using (var connection=_context.CreateConnection())
+            {
+                var values = await connection.QueryFirstOrDefaultAsync<CreateLoginDto>(query, parameters);
+                if (values != null)
+                {
+                    int UserId = values.UserID;
+                    GetCheckAppUserViewModel model = new GetCheckAppUserViewModel();
+                    model.Username = values.Username;
+                    model.Id = UserId;
+                    var token = JwtTokenGenerator.GenerateToken(model);
+                    return Ok(token);
+                }else
+                {
+                    return Ok("Başarısız");
+                }
+            }
+        }
+    }
+}
